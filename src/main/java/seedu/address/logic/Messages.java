93eb0b098f576c9998c207677f1a6b45c0c87e14
siderealmaple <e1356261@u.nus.edu>
2025-10-05 23:00:45 +0800

    Update DeleteCommand outputs

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -14,7 +14,7 @@ public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
-    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
+    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "Invalid INDEX";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_DUPLICATE_FIELDS =
                 "Multiple values specified for the following single-valued field(s): ";
@@ -48,4 +48,15 @@ public class Messages {
         return builder.toString();
     }
 
+    /**
+     * Formats the short {@code person} for display to the user.
+     */
+    public static String formatShort(Person person) {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(person.getName())
+                .append(" (");
+        person.getTags().forEach(x -> builder.append(x.toString().replaceAll("[\\[\\]]", "")));
+        builder.append(")");
+        return builder.toString();
+    }
 }
--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -23,7 +23,7 @@ public class DeleteCommand extends Command {
             + "Parameters: INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1";
 
-    public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s";
+    public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Employee deleted: %1$s";
 
     private final Index targetIndex;
 
@@ -42,7 +42,8 @@ public class DeleteCommand extends Command {
 
         Person personToDelete = lastShownList.get(targetIndex.getZeroBased());
         model.deletePerson(personToDelete);
-        return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, Messages.format(personToDelete)));
+        String feedbackString = String.format(MESSAGE_DELETE_PERSON_SUCCESS, Messages.formatShort(personToDelete));
+        return new CommandResult(feedbackString);
     }
 
     @Override
