02068bf0bf6a2f9bed6f0bcfd95e2a7e2c193f70
xavychin <xavychin@gmail.com>
2025-10-11 19:08:51 +0800

    Fix error in equals method

--- src/main/java/seedu/address/model/tag/Category.java
+++ src/main/java/seedu/address/model/tag/Category.java
@@ -7,12 +7,9 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
  * Represents an Employee's category(Role/Department/Team) in the directory.
- * Guarantees: immutable; category and value is valid as declared in {@link #isValidData(String)}
+ * Guarantees: immutable;
  */
 public class Category {
-    public static final String MESSAGE_CONSTRAINTS = "Tags names should be alphanumeric";
-    public static final String VALIDATION_REGEX = "\\p{Alnum}+";
-
     public final String category;
     public final String value;
 
@@ -24,8 +21,6 @@ public class Category {
      */
     public Category(String category, String value) {
         requireNonNull(category, value);
-        checkArgument(isValidData(category), MESSAGE_CONSTRAINTS);
-        checkArgument(isValidData(value), MESSAGE_CONSTRAINTS);
 
         this.category = category;
         this.value = value;
@@ -37,13 +32,6 @@ public class Category {
 
     public String getValue() {
         return value;
-    }
-
-    /**
-     * Returns true if a given string is a valid category or value.
-     */
-    public static boolean isValidData(String test) {
-        return test.matches(VALIDATION_REGEX);
     }
 
     @Override
