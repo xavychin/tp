35e4c8bab9736fbee0c5ec64b159fadedb1bceaf
xavychin <xavychin@gmail.com>
2025-10-11 19:15:19 +0800

    Add unit test files

---
+++ src/test/java/seedu/address/logic/commands/AssignCommandTest.java
@@ -0,0 +1,145 @@
+package seedu.address.logic.commands;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
+import seedu.address.model.AddressBook;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.*;
+import seedu.address.model.tag.Category;
+import seedu.address.model.tag.Tag;
+import seedu.address.testutil.EditPersonDescriptorBuilder;
+import seedu.address.testutil.PersonBuilder;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.*;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+public class AssignCommandTest {
+    private static final String ASSIGN_CATEGORY_STUB = "Role";
+    private static final String ASSIGN_VALUE_STUB = "Manager";
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void constructor_nullIndex_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () ->
+                new AssignCommand(null, new Category(ASSIGN_CATEGORY_STUB, ASSIGN_VALUE_STUB)));
+    }
+
+    @Test
+    public void constructor_nullCategory_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () ->
+                new AssignCommand(Index.fromZeroBased(1), null));
+    }
+
+    @Test
+    public void execute_addCategoryUnfilteredList_success() {
+        Person firstPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        Person editedPerson =
+                new PersonBuilder(firstPerson).withCategories(Arrays.asList(
+                        new Category(ASSIGN_CATEGORY_STUB, ASSIGN_VALUE_STUB))).build();
+
+        AssignCommand assignCommand = new AssignCommand(INDEX_FIRST_PERSON,
+                new Category(ASSIGN_CATEGORY_STUB, ASSIGN_VALUE_STUB));
+        String expectedMessage = String.format(AssignCommand.MESSAGE_ADD_CATEGORY_SUCCESS, "Alice Pauline; " +
+                "Phone: 94351253; " +
+                "Email: alice@example.com; " +
+                "Address: 123, Jurong West Ave 6, #08-111; " +
+                "Categories: [category='Role', value='Manager']; " +
+                "Tags: [friends]");
+
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        expectedModel.setPerson(firstPerson, editedPerson);
+
+        assertCommandSuccess(assignCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_filteredList_success() {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+
+        Person firstPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        Person editedPerson = new PersonBuilder(model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased()))
+                .withCategories(Arrays.asList(new Category(ASSIGN_CATEGORY_STUB, ASSIGN_VALUE_STUB))).build();
+
+        AssignCommand assignCommand = new AssignCommand(INDEX_FIRST_PERSON,
+                new Category(ASSIGN_CATEGORY_STUB, ASSIGN_VALUE_STUB));
+
+        String expectedMessage = String.format(AssignCommand.MESSAGE_ADD_CATEGORY_SUCCESS, "Alice Pauline; " +
+                "Phone: 94351253; " +
+                "Email: alice@example.com; " +
+                "Address: 123, Jurong West Ave 6, #08-111; " +
+                "Categories: [category='Role', value='Manager']; " +
+                "Tags: [friends]");
+
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        expectedModel.setPerson(firstPerson, editedPerson);
+
+        assertCommandSuccess(assignCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_invalidPersonIndexUnfilteredList_failure() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+        AssignCommand assignCommand = new AssignCommand(outOfBoundIndex,
+                new Category(ASSIGN_CATEGORY_STUB, ASSIGN_VALUE_STUB));
+
+        assertCommandFailure(assignCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    /**
+     * Edit filtered list where index is larger than size of filtered list,
+     * but smaller than size of address book
+     */
+    @Test
+    public void execute_invalidPersonIndexFilteredList_failure() {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+        Index outOfBoundIndex = INDEX_SECOND_PERSON;
+        // ensures that outOfBoundIndex is still in bounds of address book list
+        assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getPersonList().size());
+
+        AssignCommand assignCommand = new AssignCommand(outOfBoundIndex,
+                new Category(ASSIGN_CATEGORY_STUB, ASSIGN_VALUE_STUB));
+
+        assertCommandFailure(assignCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void equals() {
+        final AssignCommand standardCommand = new AssignCommand(INDEX_FIRST_PERSON,
+                new Category(VALID_CATEGORY_AMY, VALID_VALUE_AMY));
+
+        // same values -> returns true
+        AssignCommand commandWithSameValues = new AssignCommand(INDEX_FIRST_PERSON,
+                new Category(VALID_CATEGORY_AMY, VALID_VALUE_AMY));
+        assertTrue(standardCommand.equals(commandWithSameValues));
+
+        // same object -> returns true
+        assertTrue(standardCommand.equals(standardCommand));
+
+        // null -> returns false
+        assertFalse(standardCommand.equals(null));
+
+        // different types -> returns false
+        assertFalse(standardCommand.equals(new ClearCommand()));
+
+        // different index -> returns false
+        assertFalse(standardCommand.equals(new AssignCommand(INDEX_SECOND_PERSON,
+                new Category(VALID_CATEGORY_AMY, VALID_VALUE_AMY))));
+
+        // different remark -> returns false
+        assertFalse(standardCommand.equals(new AssignCommand(INDEX_FIRST_PERSON,
+                new Category(VALID_CATEGORY_BOB, VALID_VALUE_BOB))));
+    }
+
+}
---
+++ src/test/java/seedu/address/logic/parser/AssignCommandParserTest.java
@@ -0,0 +1,64 @@
+package seedu.address.logic.parser;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ASSIGN_CATEGORY;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ASSIGN_CATEGORY_VALUE;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.AssignCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.tag.Category;
+
+public class AssignCommandParserTest {
+
+    private final AssignCommandParser parser = new AssignCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsAssignCommand() throws Exception {
+        String userInput = "1 " + PREFIX_ASSIGN_CATEGORY + "Role " + PREFIX_ASSIGN_CATEGORY_VALUE + "Manager";
+        AssignCommand expectedCommand = new AssignCommand(
+                Index.fromOneBased(1), new Category("Role", "Manager"));
+
+        AssignCommand command = parser.parse(userInput);
+        assertEquals(expectedCommand, command);
+    }
+
+    @Test
+    public void parse_missingCategoryPrefix_usesEmptyCategory() throws Exception {
+        String userInput = "1 " + PREFIX_ASSIGN_CATEGORY_VALUE + "Manager";
+        AssignCommand expectedCommand = new AssignCommand(
+                Index.fromOneBased(1), new Category("", "Manager"));
+
+        AssignCommand command = parser.parse(userInput);
+        assertEquals(expectedCommand, command);
+    }
+
+    @Test
+    public void parse_missingValuePrefix_usesEmptyValue() throws Exception {
+        String userInput = "1 " + PREFIX_ASSIGN_CATEGORY + "Role";
+        AssignCommand expectedCommand = new AssignCommand(
+                Index.fromOneBased(1), new Category("Role", ""));
+
+        AssignCommand command = parser.parse(userInput);
+        assertEquals(expectedCommand, command);
+    }
+
+    @Test
+    public void parse_missingBothPrefixes_usesEmptyCategoryAndValue() throws Exception {
+        String userInput = "1";
+        AssignCommand expectedCommand = new AssignCommand(
+                Index.fromOneBased(1), new Category("", ""));
+
+        AssignCommand command = parser.parse(userInput);
+        assertEquals(expectedCommand, command);
+    }
+
+    @Test
+    public void parse_invalidIndex_throwsParseException() {
+        String userInput = "a " + PREFIX_ASSIGN_CATEGORY + "Role " + PREFIX_ASSIGN_CATEGORY_VALUE + "Manager";
+
+        assertThrows(ParseException.class, () -> parser.parse(userInput));
+    }
+}
